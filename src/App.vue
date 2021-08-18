087a2ef2dac10b69ec784eab6b555cc84bbc352f
Timothy Odebunmi <dev.brymes@gmail.com>
2021-08-18 16:10:57 +0100

    All done

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="App" class="font-rob">
+    <div class="container">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { createStore } from "vuex";
+// import createPersistedState from "vuex-persistedstate";
+
+// Create store
+const store = createStore({
+  modules: {
+  },
+  // plugins: [createPersistedState()],
+});
+
+export default store;
